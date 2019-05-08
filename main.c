5d55a8511ae4f5968108189f69f53486f4f66bdd
wojtas1411 <wojtas.t.1997@gmail.com>
2019-05-08 17:58:00 +0200

    update

--- main.c
+++ main.c
@@ -225,7 +225,7 @@ void send_broadcast(int msg[]){
     for(int i=0; i<nproc; i++){
         if(i!=my_id){
             MPI_Send(msg, MSG_SIZE, MPI_INT, i, MSG_TAG, MPI_COMM_WORLD);
-            printf("%d: Wysłałem do %d\n",my_id, i)
+            printf("%d: Wysłałem do %d\n",my_id, i);
         }
     }
     pthread_mutex_unlock(&send_mutex);
